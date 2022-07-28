728d377ba483f61156666b366740c48c6f05410c
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-28 10:38:08 -0230

    Add files via upload

---
+++ cube1.java
@@ -0,0 +1,18 @@
+class cube1
+{
+public static void main(String args[])
+{
+int i,num=4;
+System.out.println("enter a number:="+num);
+for(i=1;1<2;i++)
+{
+
+			System.out.println("cube of number 1 is="+(1*1*1));
+			System.out.println("cube of number 2 is="+(2*2*2));
+			System.out.println("cube of number 3 is="+(3*3*3));
+			System.out.println("cube of number 4 is="+(4*4*4));
+			}
+			}
+			}
+			
+		
