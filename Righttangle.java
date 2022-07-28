c7086164ce055c3b6575656c21795d4c28bd9f07
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-28 10:36:58 -0230

    Add files via upload

---
+++ Righttangle.java
@@ -0,0 +1,16 @@
+class Righttangle
+{
+public static void main(String args[])
+{
+int i,j,n;
+System.out.println("input number:");
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+for(i=1;i<=n;i++)
+{
+for(j=1;j<i;j++)
+System.out.println(j);
+System.out.println("");
+}
+}
+}
