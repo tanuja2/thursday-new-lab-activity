3bb064fba66afae403e393aa484160aedfb004cc
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-28 10:39:12 -0230

    Add files via upload

---
+++ Weekday.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+import java.util.Scanner;
+public class Weekday
+{
+	public static void main(String args[])
+	{
+		try (Scanner scanner=new Scanner(System.in))
+		{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter Weekday number:");
+		int Weekday=scanner.nextInt();
+		}
+	}
+}
