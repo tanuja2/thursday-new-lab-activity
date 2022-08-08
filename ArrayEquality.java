5407391510e30de13760d2a88e98173141a3edd6
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-08-08 07:41:42 -0230

    Add files via upload

---
+++ ArrayEquality.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class ArrayEquality
+{
+	public static void main(String args[])
+	{
+		int[]a=new int[]{2,4,6,8};
+		int []b=new int[]{2,4,6,8};
+		System.out.println(Arrays.equals(a,b));
+	}
+}
