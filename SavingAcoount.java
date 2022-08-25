8a1ca524ad42e47937298f5581279f0eaaf2ecfe
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-08-25 10:35:04 -0230

    Add files via upload

---
+++ SavingAcoount.java
@@ -0,0 +1,37 @@
+public class SavingAccount{
+	public static double annualInterestRate;
+	private doublesavingBalance;
+	public void SavingsAccount(){
+		annualInterestRate=0.0;
+		savingsbalance=0.0;
+	}
+	public void SavingsAccount(double intrate,doublesavbal);
+	{
+		annualInterestRate=intRate;
+		savingbalance=savbal;
+	}
+	public double calculateMonthlyInterestRate(doublenewInterestRate){
+		annualRate=new InterestRate;
+	}
+	public void setSavingsBalance(double newBal){
+		savingBalance=newbal;
+	}
+	public void get savingBalance(){
+		return savingBalance;
+	}
+	public double getannualInterestRate(){
+		return annualIntertestRate;
+	}
+	public static void main(String args[]){
+		savingAccount save1=new savingaccount();
+		saving account save2=new savingAccount();
+		save1.setsavingBalance(2000.00);
+		save2.setsavingBalance(3000.00);
+		savingaccount .modifyInterestrate(0.04);
+		save1.calculateMonthlyInterest();
+		save2.calculateMonthlyInterest();
+		System.out.println("upload balance for save1"+save1.getSavingBalance());
+		System.out.println("upload balance for save2"+save2.getSavingBalance());
+	}
+}
+		
