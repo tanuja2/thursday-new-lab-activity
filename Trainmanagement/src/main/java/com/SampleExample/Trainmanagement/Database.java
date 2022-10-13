8d4067b0491c806af1c562f7fa81d164f1665db4
Lenovo <Lenovo@DESKTOP-QQOC3T5>
2022-10-13 23:24:36 +0530

    yes it is commit.

---
+++ Trainmanagement/src/main/java/com/SampleExample/Trainmanagement/Database.java
@@ -0,0 +1,33 @@
+package com.SampleExample.Trainmanagement;
+import java.sql.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+public class Database {
+		private static Connection con = null;
+	 	private static Properties props = new Properties();
+	 	
+	 
+	 //ENSURE YOU DON'T CHANGE THE BELOW CODE WHEN YOU SUBMIT
+	 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	 	    try{
+	 			
+	 			FileInputStream fis = null;
+	 			fis = new FileInputStream("database.properties");
+	  			props.load(fis);
+	 			
+	 			// load the Driver Class
+	 			Class.forName(props.getProperty("DB_DRIVER_CLASS"));
+	 
+	 			// create the connection now
+	            con = DriverManager.getConnection(props.getProperty("DB_URL"),props.getProperty("DB_USERNAME"),props.getProperty("DB_PASSWORD"));
+	    	    }
+	    	    catch(IOException e){
+	    	        e.printStackTrace();
+	    	    }
+	    		return con;	
+	    	}
+	    }
